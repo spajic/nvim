8e0795bdd1de325ea6443ebb845990578bc92d3a
Alexey Vasilyev <bestspajic@gmail.com>
2023-02-24 13:25:37 +0300

    Add fugitive for git integration

--- lua/spajic/packer.lua
+++ lua/spajic/packer.lua
@@ -25,4 +25,6 @@ return require('packer').startup(function(use)
   }
 
   use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+  use('tpope/vim-fugitive')
+  
 end)
