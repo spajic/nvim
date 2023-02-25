08d29680a2a4447db7220fb22036bfaa0c75259b
Alexey Vasilyev <bestspajic@gmail.com>
2023-02-26 00:12:01 +0300

    Setup LSP diagnostics with virtualText

--- lua/spajic/remap.lua
+++ lua/spajic/remap.lua
@@ -1,2 +1,15 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>ex", vim.cmd.Ex) -- go to explorer (rw171)
+
+-- Configure inline diagnostics
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = {
+            prefix = "~",
+            spacing = 0,
+        },
+        signs = false,  
+        underline = true, 
+    }
+)
+vim.keymap.set("i", "jj", "<Esc>") -- jj for Esc from Insert mode 
