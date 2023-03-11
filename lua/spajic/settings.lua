8f22a1d83b8a22aaa24ec99d3ea3bc66d4127aa0
Alexey Vasilyev <bestspajic@gmail.com>
2023-03-12 00:00:07 +0300

    Add settings for clipboard, mouse, search, scroll

--- lua/spajic/settings.lua
+++ lua/spajic/settings.lua
@@ -1,3 +1,13 @@
 vim.opt.signcolumn = 'yes'
 vim.o.number = true -- enable line numbers
 vim.o.relativenumber = true -- enable relative line numbers
+vim.o.clipboard = 'unnamedplus' -- Sync clipboard between OS and Neovim.
+vim.o.mouse = 'a' -- enable mouse, sometimes it is convenient
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 10 -- start scrolling 10 lines before reaching top or bottom of the page
+vim.opt.colorcolumn = '80' -- visual reminder to not have long string
