6d6b7e2e3600337cf9562b2709d2f0d04b76ac66
Alexey Vasilyev <bestspajic@gmail.com>
2023-03-12 00:20:21 +0300

    Setup some remaps from primeagen

--- lua/spajic/remap.lua
+++ lua/spajic/remap.lua
@@ -14,3 +14,19 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 )
 vim.keymap.set("i", "jj", "<Esc>") -- jj for Esc from Insert mode
 vim.keymap.set("i", "ww", "<Esc>:w<CR>") -- ww to save and go to Normal mode 
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- move selected lines up
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- move selected lines down
+vim.keymap.set("n", "J", "mzJ`z") -- do not move cursor after applying J
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz") -- move half page down, cursor stays
+vim.keymap.set("n", "<C-u>", "<C-u>zz") -- move half page up, cursor stays
+
+vim.keymap.set("n", "n", "nzzzv") -- search result stays in the middle
+vim.keymap.set("n", "N", "Nzzzv") -- search result stays in the middle
+vim.keymap.set("n", "<CR>", ":noh<CR>")
+vim.keymap.set( -- replace a word under a cursor
+  "n",
+  "<leader>s",
+  [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]
+)
