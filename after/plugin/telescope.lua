9c46006b5bfeb576fd968b23bbd4513b4cbd3c84
Alexey Vasilyev <bestspajic@gmail.com>
2023-03-12 00:21:58 +0300

    Config remaps and long paths display for Telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -9,23 +9,30 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 -- TODO: Rework my comments to desc attribute
-vim.keymap.set('n', '<leader>fa', builtin.find_files, {})       -- [f]ind [a]ll files (not only git)
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})        -- [f]ind [g]rep
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})          -- [f]ind [b]uffers
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})        -- [f]ind [h]elp
-vim.keymap.set('n', '<leader>fz', function()                    -- [f]ind fu[z]zy
+vim.keymap.set('n', '<leader>fa', builtin.find_files, {desc = '[f]ind [a]ll files (not only git)'})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = '[f]ind [g]rep'})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = '[f]ind [b]uffers'})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = '[f]ind [h]elp'})
+vim.keymap.set('n', '<leader>fz', function()
   builtin.grep_string({search = ''})
-end)
-vim.keymap.set('n', '<leader>fc', builtin.grep_string, {})      -- [f]ind term under [c]ursor
-vim.keymap.set('n', '<leader>ff', function() --                 -- [f]ind [f]iles in file browser
+end, {desc = '[f]ind fu[z]zy'})
+vim.keymap.set('n', '<leader>fw', builtin.grep_string, {desc = '[f]ind [w]ord under the cursor'})
+vim.keymap.set('n', '<leader>ff', function()
   vim.cmd(':Telescope file_browser')
-end)
-vim.api.nvim_set_keymap("n", "<leader><leader>", "<Cmd>lua require('telescope').extensions.frecency.frecency({ workspace = 'CWD' })<CR>", {noremap = true, silent = true})
-vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind [D]iagnostics' })
+end, {desc = '[f]ind [f]iles in file browser'})
+vim.api.nvim_set_keymap("n", 
+  "<leader><leader>", 
+  "<Cmd>lua require('telescope').extensions.frecency.frecency({ workspace = 'CWD', path_display={shorten = {len = 3}} })<CR>",
+  {noremap = true, silent = true}
+)
+vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[f]ind [d]iagnostics' })
 
 -- You dont need to set any of these options. These are the default ones. Only
 -- the loading is important
 require('telescope').setup {
+  defaults = { -- SEE :h telescope.defaults
+    path_display = {'truncate'},
+  },
   extensions = {
     fzf = {
       fuzzy = true,                    -- false will only do exact matching
